215dc11caa42927ba2db37eac1cfb496a09cdbd0
Erikka A <114741859+Akkirea@users.noreply.github.com>
2023-03-10 10:25:25 -0700

    Update Files

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-document.getElementById('btn').addEventListener('click', function(e){
-    e.preventDefault()
-    getColorsFromApi()
-})
+const btn = document.getElementById('btn')
+const main = document.getElementById('main')
 
-getColorsFromApi() {
-    const seedColor = document.getElementById('color-picker').value
-    const scheme = document.getElementById('scheme-picker').value
-
-    fetch(`https://www.thecolorapi.com/schemes?hex=${seedColor.slice(1,7)}&mode=${scheme}`)
+btn.addEventListener('click', function(event){
+    event.preventDefault()
+    const hex = document.getElementById('selected-color').value.slice(1,7)
+    const mode = document.getElementById('scheme-picker').value
+    
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}&count=4`)
         .then(res => res.json())
         .then(data => {
-            let colorArray = []
-            for (let i=0; i<data.colors.length; i++){
-                colorArray.push(data.colors[i].hex.value)
+            document.getElementById("color-seed").style.background = "#" + hex
+            document.getElementById("hex-seed").textContent = "#" + hex.toUpperCase()
+            for(let i=0; i<data.colors.length;i++) {
+                document.getElementById(`color-${i+1}`).style.background = data.colors[i].hex.value
+                document.getElementById(`hex-${i+1}`).textContent = data.colors[i].hex.value
             }
-            let html = ''
-            for (let color of colorArray){
-                html +=`
-                <div class="mid-section">
-                <div id="color-block" class="color-block" style="background-color:${color}"></div>
-                <p>${color}</p>
-                </div>`
-            }
-            
-            document.querySelector('main').innerHTML = html
         })
-}
+})
 
-    
+
+
 
