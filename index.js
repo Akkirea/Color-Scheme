9129c0987fecfc4a827cfa4161a813c49707151e
Erikka A <114741859+Akkirea@users.noreply.github.com>
2023-03-14 12:13:13 -0700

    Commited JS

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
-const btn = document.getElementById('btn')
 
-
-btn.addEventListener('click', function(event){
+document.getElementById('btn').addEventListener('click', function (event) {
     event.preventDefault()
-    const hex = document.getElementById('selected-color').value.slice(1,7)
+    fetchApi()
+})
+
+function fetchApi() {
     const mode = document.getElementById('scheme-picker').value
-    
+    const hex = document.getElementById('selected-color').value.slice(1, 7)
+
     fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}&count=4`)
         .then(res => res.json())
         .then(data => {
             document.getElementById("color-seed").style.background = "#" + hex
             document.getElementById("hex-seed").textContent = "#" + hex.toUpperCase()
-            for(let i=0; i<data.colors.length;i++) {
-                document.getElementById(`color-${i+1}`).style.background = data.colors[i].hex.value
-                document.getElementById(`hex-${i+1}`).textContent = data.colors[i].hex.value
+            for (let i = 0; i < data.colors.length; i++) {
+                document.getElementById(`color-${i + 1}`).style.background = data.colors[i].hex.value
+                document.getElementById(`hex-${i + 1}`).textContent = data.colors[i].hex.value
             }
         })
-})
+}
 
-
-document.querySelector(".hex-codes").addEventListener('click', function(){
-    let copyText = document.querySelector(".colors")
-    navigator.clipboard.writeText(copyText).then(()=>{
+document.querySelector(".hex-codes").addEventListener('click', function(e) {
+    let copyText = document.getElementById("hex-seed").textContent
+    navigator.clipboard.writeText(copyText).then(() => {
         alert("Copied to clipboard")
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
